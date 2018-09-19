83a29a637600bc5c167ef21305b1898467a33c47
wrenth04 <wrenth04@gmail.com>
2018-09-19 23:13:38 +0800

    update ptt

--- plugins/ptt.js
+++ plugins/ptt.js
@@ -6,20 +6,22 @@ const filter = 'www.ptt.cc/bbs';
 module.exports = {filter, action};
 
 function action(uri) {
-    return axios.get(uri, {headers: {cookie: 'over18=1'}})
-      .then(res => cheerio.load(res.data))
-      .then($ => {
-        const title = $('title').text();
-        const imgs = $('#main-content a')
-        .text()
-        .split('http')
-        .filter(e => e.indexOf('imgur') != -1)
-        .map(e => e.split('/')[3].split('.')[0])
-        .map(id => ({
+  return axios.get(uri, {headers: {cookie: 'over18=1'}})
+    .then(res => cheerio.load(res.data))
+    .then($ => {
+      const title = $('title').text();
+      const imgs = [];
+      $('#main-content a').each((i, e) => {
+        const href = $(e).attr('href');
+        if(href.indexOf('imgur') == -1) return;
+        const id = href.split('/')[3].split('.')[0];
+        imgs.push({
           src: `https://i.imgur.com/${id}h.jpg`,
           link: `https://i.imgur.com/${id}.jpg`
-        }));
-        return {title, imgs};
-      })
-      .then(imgwall);
+        });
+      });
+
+      return {title, imgs};
+    })
+    .then(imgwall);
 }
