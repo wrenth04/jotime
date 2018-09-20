3f2a3405837431ff992c1901e3bd5e118fe44a03
wrenth04 <wrenth04@gmail.com>
2018-09-20 10:18:08 +0800

    update ck101

--- plugins/ck101.js
+++ plugins/ck101.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
-const {imgwall} = require('./utils');
+const {imgwall, findIg} = require('./utils');
 
 const filter = 'ck101';
 module.exports = {filter, action};
@@ -8,8 +8,9 @@ module.exports = {filter, action};
 function action(uri) {
   return axios.get(uri)
     .then(res => cheerio.load(res.data))
-    .then($ => {
-      const title = $('title').text();
+    .then(findIg)
+    .then(({$, igs}) => {
+      const title = $('meta[property="og:title"]').attr('content');
       const imgs = [];
       $('img[itemprop="image"]').each((i, e) => {
         if(i == 0 || i > 10) return;
@@ -17,18 +18,11 @@ function action(uri) {
         imgs.push({src, link});
       });
 
-      const igs = [];
-      $('a').each((i, e) => {
-        const link = $(e).attr('href');
-        if(!link || link.indexOf('instagram.com') == -1) return;
-        igs.push(link);
-      });
+      const last = imgs.length > 1 ? imgs[imgs.length - 1] : imgs[0];
+      last.text = 'view more';
+      last.link = uri;
 
-      if(igs.length > 0) {
-        imgs[0].text = 'ig';
-        imgs[0].link = igs[0];
-      }
-      return {title, imgs};
+      return {title, imgs, igs};
     })
     .then(imgwall);
 }
--- plugins/utils.js
+++ plugins/utils.js
@@ -1,25 +1,43 @@
 
-module.exports = {imgwall};
+module.exports = {imgwall, findIg};
+
+function findIg($) {
+  const igs = [];
+  $('a').each((i, e) => {
+    const href = $(e).attr('href') || '';
+    if(href.indexOf('instagram.com') == -1 || href.indexOf('/p/') != -1) return;
+    igs.push(href);
+  });
+  return {$, igs};
+}
 
 /*
   input: 
   {
     title: 'title text',
     imgs: [{
-      text: 'img label',
+      text: 'img label max 12 char',
       src: 'img url max to 1024x1024',
       link: 'img link'
-    }]
+    }],
+    igs: ['ig link']
   }
 */
-function imgwall({imgs=[], title=""}) {
-  var cols = [];
+function imgwall({imgs=[], title="", igs=[]}) {
+  if(igs.length > 0) {
+    imgs[0].text = 'ig';
+    imgs[0].link = igs[0];
+  }
+
+  const cols = [];
+
   for(var i = 0 ; i < Math.min(10, imgs.length); i++) {
+    const label = imgs[i].text;
     cols.push({
       "imageUrl": imgs[i].src || imgs[i],
       "action": {
         "type": "uri",
-        "label": imgs[i].text || (i+1)+'',
+        label,
         "uri": imgs[i].link || title
       }
     });
