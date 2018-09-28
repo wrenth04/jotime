d41260db43e371612a15f54c5e26d7995f883e96
wrenth04 <wrenth04@gmail.com>
2018-09-28 15:12:28 +0800

    add funnywithyou

---
+++ plugins/funnywithyou.js
@@ -0,0 +1,23 @@
+const {ssl, debug, $, imgwall, findLinks} = require('./utils');
+
+const filter = 'funnywithyou.life';
+module.exports = {filter, action};
+
+function action(uri) {
+  return $(uri)
+    .then(findLinks)
+    .then(({$, links}) => {
+      const title = $('title').text();
+      const imgs = [];
+      $('.figure img').each((i, e) => {
+        const src = 'http://www.funnywithyou.life' + $(e).attr('src');
+        imgs.push({
+          src: ssl(src),
+          link: src
+        });
+      });
+
+      return {title, imgs, links};
+    })
+    .then(imgwall);
+}
