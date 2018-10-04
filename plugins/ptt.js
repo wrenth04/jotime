2fb4970c6659e7e02aa6ce052ca6dac8fb0d4602
wrenth04 <wrenth04@gmail.com>
2018-10-04 14:04:04 +0800

    add ptt daemon

--- plugins/ptt.js
+++ plugins/ptt.js
@@ -1,7 +1,39 @@
-const {viewMore, $, imgwall, findLinks} = require('./utils');
+const {pushMsg, dbInstance, viewMore, $, imgwall, findLinks} = require('./utils');
+const db = dbInstance('ptt');
+const DELAY = 15*60*1000;
 
 const filter = 'www.ptt.cc/bbs';
 module.exports = {filter, action};
+
+var steam = {};
+db.get('steam')
+  .then(data => steam = data || steam)
+  .then(() => {
+    steamDaemon();
+    setInterval(steamDaemon, DELAY);
+  });
+
+function steamDaemon() {
+  $('https://www.ptt.cc/bbs/Steam/index.html')
+    .then($ => {
+      const links = [];
+      $('.title a').each((i, e) => {
+        const $e = $(e);
+        const title = $e.text() || '';
+        if(title.indexOf('[限免]') == -1) return;
+        links.push($e.attr('href'));
+      });
+      if(links.length == 0) return;
+      const last = links.reverse()[0];
+      if(steam.last == last) return;
+      steam.last = last;
+      db.put({path: 'steam', json: {last}});
+      pushMsg(steam.to, {
+        type: 'text',
+        text: `https://www.ptt.cc${last}`
+      });
+    });
+}
 
 function action(uri) {
   return $(uri)
