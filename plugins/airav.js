a8a69d9d6e68d1c134c36f7c19cd26a44a14a6d3
wrenth04 <wrenth04@gmail.com>
2018-10-06 00:09:37 +0800

    update airav

--- plugins/airav.js
+++ plugins/airav.js
@@ -1,17 +1,18 @@
-const {$, debug} = require('./utils');
+const {$, debug, ssl} = require('./utils');
 
 const filter = 'airav.cc';
 module.exports = {filter, action};
 
 function action(uri) { 
+  uri = uri.replace('playon', 'EmbeddedVideo').replace('m.', '');
   return $(uri)
     .then($ => {
       const uri = $('source[type="video/mp4"]').attr('src');
-      const img = $('meta[property="og:image"]').attr('content');
+      const img = $('meta[property="og:image"]').attr('content') || $('video').attr('poster');
       return {
         "type": "video",
         "originalContentUrl": uri,
-        "previewImageUrl": img
+        "previewImageUrl": ssl(img)
       };
     });
 }
