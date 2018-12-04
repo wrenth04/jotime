2f3b0f598363eb487c96109c8cae05615df4a9d6
wrenth04 <wrenth04@gmail.com>
2018-12-04 16:45:07 +0800

    update imgur

--- plugins/ptt.js
+++ plugins/ptt.js
@@ -44,10 +44,10 @@ function action(uri) {
       $('#main-content>a').each((i, e) => {
         const href = $(e).attr('href');
         if(href.indexOf('imgur') == -1) return;
-        const id = href.split('/')[3].split('.')[0];
+        //const id = href.split('/')[3].split('.')[0];
         imgs.push({
-          src: `https://i.imgur.com/${id}h.jpg`,
-          link: `https://i.imgur.com/${id}.jpg`
+          src: href,
+          link: href
         });
       });
 
