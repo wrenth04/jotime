82546fefa3100960083d3c926e6905c256de84ab
wei <wrenth04@gmail.com>
2020-05-14 11:47:44 +0800

    update eats

--- plugins/eats.js
+++ plugins/eats.js
@@ -10,31 +10,31 @@ var restaurant = [];
 updateRestaurant();
 setInterval(updateRestaurant, DELAY);
 function updateRestaurant() {
-  db.get('restaurant').then(data => restaurant = data || restaurant);
-  console.log(restaurant);
+  db.get('restaurant').then(data => {
+    restaurant = data || restaurant
+    console.log(restaurant);
+  });
 }
 
 function action(uri) {
-  return new Promise(reslove => {
-    reslove({
-      type: 'template',
-      altText: '吃什麼',
-      template: {
-        type: 'carousel',
-        columns: restaurant.shuffle().slice(0, 10).map(({title, map, img}) => ({
-          text: title,
-          thumbnailImageUrl: img,
-          actions: [{
-            type: 'uri',
-            label: 'map',
-            uri: map
-          }, {
-            type: 'message',
-            label: `${title}+1`,
-            text:`${title}+1` 
-          }]
-        }))
-      }
-    });
+  return Promise.resolve({
+    type: 'template',
+    altText: '吃什麼',
+    template: {
+      type: 'carousel',
+      columns: restaurant.shuffle().slice(0, 10).map(({title, map, img}) => ({
+        text: title,
+        thumbnailImageUrl: img,
+        actions: [{
+          type: 'uri',
+          label: 'map',
+          uri: map
+        }, {
+          type: 'message',
+          label: `${title}+1`,
+          text:`${title}+1` 
+        }]
+      }))
+    }
   });
 }
